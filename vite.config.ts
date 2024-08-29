b52d3cf15fe0186dd8eaa4037155ca75affd2edd
developermong2 <developermong2@gmail.com>
2024-08-29 09:54:01 +0900

    Feat : vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000, // 원하는 포트 번호
-  },
-  plugins: [react()]
-})
+  // ex base: '/my_project/',
+  base: '/<use_cornerstonejs>/',
+  plugins: [react()],
+});
