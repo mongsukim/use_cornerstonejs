1c28b23d7b9f9f73f8c57e69369f332d0deec4f9
developermong2 <developermong2@gmail.com>
2024-08-29 10:04:00 +0900

    Etc : 빌드 명령어 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // ex base: '/my_project/',
-  base: '/<use_cornerstonejs>/',
+  server: {
+    port: 3000, // 원하는 포트 번호
+  },
   plugins: [react()],
 });
